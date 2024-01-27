0abf9254b1ce26a0a3df6ad19540c996e610a418
WASEEM KHAN <waseem3khan333@gmail.com>
2024-01-27 22:44:03 +0500

    initial commit

--- client/src/component/Layout.jsx
+++ client/src/component/Layout.jsx
@@ -31,9 +31,9 @@ const Layout = () => {
                     <p className='text-[#657073] py-5 text-center text-2xl px-10'>WedBook is a Pakistani Wedding Planning Website and app where you can find the best wedding vendors, with prices and reviews at the click of a button. Whether you are looking to register wedding service in Pakistan or looking for the top venues, cooks ,decorator or caterer. WedMeGood can help you solve your wedding planning woes through its unique features. With a checklist, detailed vendor list, inspiration gallery and blog - you won't need to spend hours planning a wedding anymore.</p>
                 </div>
             </div>
-            <div className='bg-img2 text-white flex flex-col justify-center items-center p-10'>
-                <h1 className='text-3xl font-semibold'>Celebrate with Us</h1>
-                <p className='text-2xl mt-6'>As the premier event planning company in the area,Each event and client is unique and we believe our services should be as well</p>
+            <div className='bg-img2  text-white flex flex-col justify-center items-center p-10'>
+                <h1 className='text-4xl font-semibold'>Celebrate with Us</h1>
+                <p className='text-2xl  mt-6'>As the premier event planning company in the area,Each event and client is unique and we believe our services should be as well</p>
             </div>
         </>
     )
--- client/src/component/Register.jsx
+++ client/src/component/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
 
     return (
         <>
-            <div className=' container mx-auto flex flex-col md:flex-row  justify-around shadow-emerald-100 bg-dark-green text-white'>
+            <div className=' container mx-auto  -translate-y-10 flex flex-col md:flex-row  justify-around shadow-emerald-100 bg-dark-green text-white'>
                 <div className='h-[15vh]  flex flex-col justify-center items-center '>
                     <h1 className='text-2xl font-semibold'>250+</h1>
                     <h1 className='text-xl'>Registered Venu</h1>
